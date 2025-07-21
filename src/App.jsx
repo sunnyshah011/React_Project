8ff00708781fccd26da73095e430d2802a2bfad4
sunnyshah011 <sunnyshah35350@gmail.com>
2025-07-21 20:30:02 +0545

    navbar change and add hero

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Product from "./pages/Product";
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]" >
+    <div className="px-5 sm:px-[5vw] md:px-[8vw] lg:px-[12vw] h-[200vh]" >
       <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/component/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      dfsa
+    </div>
+  )
+}
+
+export default Hero
