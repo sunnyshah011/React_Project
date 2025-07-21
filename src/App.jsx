096b9387fe630ec327c2e6d04b44d66f167d880b
sunnyshah011 <sunnyshah35350@gmail.com>
2025-07-21 12:00:10 +0545

    FTS

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+import "./index.css";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Order from "./pages/Order";
+import Placeorder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] w-full bg-black h-[100vh]" >
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/placeorder" element={<Placeorder />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[50px] bg-white">
+        
+    </div>
+  )
+}
+
+export default Navbar
