f586dcfdc84e249d86f0cad66e46038b66594092
sunnyshah011 <sunnyshah35350@gmail.com>
2025-07-21 12:22:40 +0545

    change

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Product from "./pages/Product";
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] w-full bg-black h-[100vh]" >
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] w-full h-[100vh]" >
       <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} />
